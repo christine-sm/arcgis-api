6f59d4ad5be8aacfed9659451916a474b7f32613
Christine Movius <christine.movius@gmail.com>
2016-04-20 22:21:01 -0400

    Fix zoom in to maximum and declare map as global variable for easier debugging.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("I'm connected");
-
+var map;
 require(["esri/map", "dojo/domReady!"], function(Map) {
   map = new Map("mapDiv", {
     center: [-77.0884068, 38.8908447],
-    zoom: 17,
+    zoom: 16,
     basemap: "streets"
   });
 });
