e2a933923f33605b5a0b8fac61532ae191d6d04e
Christine Movius <christine.movius@gmail.com>
2016-04-21 13:53:08 -0400

    Delete code for old map.

--- script.js
+++ script.js
@@ -7,9 +7,4 @@ require([
     arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "mapDiv").then(function (response) {
       map = response.map;
   });
-  // map = new Map("mapDiv", {
-  //   center: [-77.0884068, 38.8908447],
-  //   zoom: 16,
-  //   basemap: "streets"
-  // });
 });
