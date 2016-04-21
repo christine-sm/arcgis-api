f7a10c1f21ccac168aaaa762af178d561c09edc7
Christine Movius <christine.movius@gmail.com>
2016-04-20 22:05:24 -0400

    Load modules, ensure the DOM is available, and create a map that is centered on Blue Raster's address.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("I'm connected");
+
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  map = new Map("mapDiv", {
+    center: [-77.0884068, 38.8908447],
+    zoom: 17,
+    basemap: "streets"
+  });
+});
