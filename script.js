313019881c4809a836c08087c9099f48c848de89
Christine Movius <christine.movius@gmail.com>
2016-04-21 19:28:29 -0400

    Move commented out code to bottom.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// console.log("I'm connected");
-// var map;
-// require([
-//   "esri/map",
-//   "esri/arcgis/utils",
-//   "dojo/domReady!"], function(Map, arcgisUtils) {
-//     arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "mapDiv").then(function (response) {
-//       map = response.map;
-//   });
-// });
-
 var map;
 require([
     "dojo/dom",
@@ -80,3 +69,14 @@ require([
     }, dom.byId("measurementDiv"));
     measurement.startup();
   });
+
+  // console.log("I'm connected");
+  // var map;
+  // require([
+  //   "esri/map",
+  //   "esri/arcgis/utils",
+  //   "dojo/domReady!"], function(Map, arcgisUtils) {
+  //     arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "mapDiv").then(function (response) {
+  //       map = response.map;
+  //   });
+  // });
