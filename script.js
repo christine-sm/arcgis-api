b3869b17a0f1f593019c637f8721ab11d57fa75c
Christine Movius <christine.movius@gmail.com>
2016-04-21 09:03:12 -0400

    Replace standard map with web map that has a layer with two pinpoints- Blue Raster and Ga.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 console.log("I'm connected");
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-    center: [-77.0884068, 38.8908447],
-    zoom: 16,
-    basemap: "streets"
+require([
+  "esri/map",
+  "esri/arcgis/utils",
+  "dojo/domReady!"], function(Map, arcgisUtils) {
+    arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "mapDiv").then(function (response) {
+      map = response.map;
   });
+  // map = new Map("mapDiv", {
+  //   center: [-77.0884068, 38.8908447],
+  //   zoom: 16,
+  //   basemap: "streets"
+  // });
 });
