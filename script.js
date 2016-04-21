8b0b40f8408486b89248175c39866f3b18019e90
Christine Movius <christine.movius@gmail.com>
2016-04-21 14:32:00 -0400

    Trying to implement directions widget, but encountering a permissions issue even after I sign into my ArcGIS online account.

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ var map;
 require([
   "esri/map",
   "esri/arcgis/utils",
-  "dojo/domReady!"], function(Map, arcgisUtils) {
-    arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "mapDiv").then(function (response) {
+  "esri/dijit/Directions",
+  "dojo/parser",
+  "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
+  "dojo/domReady!"
+], function(Map, arcgisUtils, Directions, parser) {
+    parser.parse();
+    var map;
+    arcgisUtils.createMap("fe8a35a42b57430793313245886baf21", "map").then(function (response) {
       map = response.map;
-  });
+      var directions = new Directions({
+        map: map
+      }, "dir");
+      directions.startup();
+    });
 });
